78ea3a1016b6bfa21e4020d6ff94579a16b2793f
y.pearl84 <pearl@glocalizeinc.com>
2021-02-11 15:59:31 -0800

    Add database info, Add insert function

--- application/controllers/ad_api.php
+++ application/controllers/ad_api.php
@@ -6,6 +6,7 @@ class Ad_api extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model('ad_model');
+        $this->load->library('form_validation');
     }
     
     function index() {
@@ -13,7 +14,37 @@ class Ad_api extends CI_Controller {
     }
 
     function insert() {
-        echo "insert";
+        
+        $this->form_validation->set_rules("partner_id", "partner_id", "required");
+        $this->form_validation->set_rules("duration", "duration", "required");
+        $this->form_validation->set_rules("ad_content", "ad_content", "required");
+
+        $array = array();
+        if($this->form_validation->run()) {
+
+            $data = array(
+                'partner_id' => trim($this->input->post('partner_id')),
+                'duration'  => trim($this->input->post('duration')),
+                'ad_content'  => trim($this->input->post('ad_content'))
+            );
+
+            $this->ad_model->insert_data($data);
+            
+            $array = array(
+                'success'  => true
+            );
+
+        } else {
+
+            $array = array(
+                'error'    => true,
+                'partner_id_error' => form_error('partner_id'),
+                'duration_error' => form_error('duration'),
+                'ad_content' => form_error('ad_content')
+            );
+        }
+
+        echo json_encode($array, true);
     }
 
     function delete() {
--- application/models/ad_model.php
+++ application/models/ad_model.php
@@ -1,4 +1,12 @@
 <?php
-class Api_model extends CI_Model
-{
+class Ad_model extends CI_Model {
+
+    function insert_data($data) {
+        $this->db->insert('ad_campaign', $data);
+        if($this->db->affected_rows() > 0) {
+            return true;
+        } else {
+            return false;
+        }  
+    }
 }
