6f9035569cd4b4d9642f2865b30d6879a1891bf8
y.pearl84 <pearl@glocalizeinc.com>
2021-02-11 16:39:27 -0800

    Change db config

--- application/config/database.php
+++ application/config/database.php
@@ -77,7 +77,7 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',
+	'password' => '1234',
 	'database' => 'thetundra',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
@@ -92,5 +92,6 @@ $db['default'] = array(
 	'compress' => FALSE,
 	'stricton' => FALSE,
 	'failover' => array(),
-	'save_queries' => TRUE
+	'save_queries' => TRUE,
+	'port' => 3306
 );
--- application/models/ad_model.php
+++ application/models/ad_model.php
@@ -1,6 +1,13 @@
 <?php
 class Ad_model extends CI_Model {
 
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+        $this->load->database('default', TRUE);
+    }
+
     function insert_data($data) {
         $this->db->insert('ad_campaign', $data);
         if($this->db->affected_rows() > 0) {
